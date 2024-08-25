43cb3e49d749212c14eb06e4799e876647ee6b17
IT <icode67658@gmail.com>
2024-08-25 23:24:23 +0800

    Added register

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,8 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { AccountService } from './_services/account.service';
-import { HomeComponent } from "./home/home.component";
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
@@ -13,14 +12,10 @@ import { HomeComponent } from "./home/home.component";
   styleUrl: './app.component.css',
 })
 export class AppComponent implements OnInit {
-  http = inject(HttpClient);
   private accountService = inject(AccountService);
-  title = 'Dating App';
-  users: any;
 
   ngOnInit(): void {
     this.setCurrentUser();
-    this.getUsers();
   }
 
   setCurrentUser() {
@@ -31,13 +26,5 @@ export class AppComponent implements OnInit {
     const user = JSON.parse(userString);
 
     this.accountService.currentUser.set(user);
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: (response) => (this.users = response),
-      error: (error) => console.log(error),
-      complete: () => {},
-    });
   }
 }
